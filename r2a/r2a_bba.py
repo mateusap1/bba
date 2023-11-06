db84e290c24259ecd9087984925b34a9967c2fa2
leorb99 <leo.rb1999@gmail.com>
2023-11-06 10:46:44 -0300

    calculo de throughput e metodo que desenha grafico

--- r2a/r2a_bba.py
+++ r2a/r2a_bba.py
@@ -1,7 +1,9 @@
 from r2a.ir2a import IR2A
 from player.parser import *
-import time
+import matplotlib.pyplot as plt
 from statistics import mean
+import time
+from base.timer import Timer
 import random
 
 
@@ -12,6 +14,9 @@ class R2A_BBA(IR2A):
     def __init__(self, id):
         IR2A.__init__(self, id)
         self.qi = []
+        self.throughputs = []
+        self.timer = Timer.get_instance()
+        self.request_time = 0
         self.rate = 0
         self.reservoir = 5
         self.cushion = 10
@@ -21,6 +26,7 @@ class R2A_BBA(IR2A):
         self.max_buf = 60
 
     def handle_xml_request(self, msg):
+        self.request_time = time.perf_counter()
         self.send_down(msg)
 
     def handle_xml_response(self, msg):
@@ -35,13 +41,11 @@ class R2A_BBA(IR2A):
         print()
         print(self.qi)
         print()
-        self.rate_min = 0
         self.rate_max = len(self.qi) - 1
 
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
-
         if self.rate == len(self.qi) - 1:
             rate_plus = self.rate_max
         else:
@@ -61,17 +65,64 @@ class R2A_BBA(IR2A):
             rate_next = rate_plus
         else:
             rate_next = self.rate
+
         self.rate = rate_next
         msg.add_quality_id(self.qi[self.rate])
+        
+        self.request_time = time.perf_counter()
         self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
         self.buffer_size = self.whiteboard.get_amount_video_to_play()
+
+        current_time = self.timer.get_current_time()
+        t = time.perf_counter() - self.request_time
+        self.throughputs.append((current_time, (msg.get_bit_length() / t)))
+ 
         self.send_up(msg)
 
     def initialize(self):
         pass
 
     def finalization(self):
-        print(self.whiteboard.get_playback_qi())
-        print(self.whiteboard.get_playback_buffer_size())
+        self.draw_graphs(self.whiteboard.get_playback_qi(), self.throughputs,
+                         "QIvsThroughput", "Quality Index vs. Throughput", "QI")
+        self.draw_graphs(self.whiteboard.get_playback_buffer_size(), self.throughputs,
+                         "BuffervsThroughput", "Buffer Size vs. Throughput", "Buffer")
+        self.draw_graphs(self.whiteboard.get_playback_buffer_size(), 
+                         self.whiteboard.get_playback_qi(), "BuffervsQI", 
+                         "Buffer Size vs. Quality Index", "Buffer", "QI", tp=False)
+
+    def draw_graphs(self, data1, data2, file_name, title, y_axis_left,
+                    y_axis_right="Mbps", x_axis="execution_time (s)", tp=True):
+        x1 = [item[0] for item in data1]
+        y1 = [item[1] for item in data1]
+        
+        x2 = [item[0] for item in data2]
+        y2 = [item[1] for item in data2]
+        
+        _, ax1 = plt.subplots()
+        ax1.plot(x1, y1, color="blue")
+        ax1.set_xlabel(x_axis)
+        ax1.set_ylabel(y_axis_left, color="blue")
+        ax1.tick_params(axis="y", labelcolor="blue")
+        plt.ylim(min(y1), max(y1) * 4 / 3)
+
+        if tp:
+            ax2 = ax1.twinx()
+            ax2.vlines(x2, [0], y2, color="red")
+            ax2.set_ylabel(y_axis_right, color="red")
+            ax2.tick_params(axis="y", labelcolor="red")
+            plt.ylim(min(y2), max(y2) * 4 / 3)
+        else:
+            ax2 = ax1.twinx()
+            ax2.set_ylabel(y_axis_right, color="green")
+            ax2.tick_params(axis="y", labelcolor="green")
+            plt.ylim(min(y2), max(y2) * 4 / 3)
+            ax1.plot(x2, y2, color="green")
+
+        plt.title(title)
+        plt.savefig(f'./results/{file_name}.png')
+        plt.clf()
+        plt.cla()
+        plt.close()
